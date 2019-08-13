5e13e06e8028f1a34a6d763899b721b2ad2da9b0
Alexandru Ioana <alexandru.ioana@lummetry.ai>
2019-08-13 15:31:37 +0300

    validation set creation

--- tagger/crawler/EY_data/ey_preprocessing.py
+++ tagger/crawler/EY_data/ey_preprocessing.py
@@ -1,19 +1,23 @@
+import random
 import pandas as pd
 
 from collections import Counter
 from libraries.logger import Logger
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import TweetTokenizer
+
+random.seed(1)
 
 def flatten_list(a):
   return [item for sublist in a for item in sublist]
 
 class EY_Data(object):  
-  def __init__(self, logger, folder_name, occurence_threshold=0.25):
+  def __init__(self, logger, folder_name, occurence_threshold=0.25, validation_set_length=10):
     self.logger = logger
     self.folder_name = folder_name
     self.occurence_threshold = occurence_threshold
+    self.validation_set_length = validation_set_length
     
-    self.tokenizer = RegexpTokenizer(r'\w+')
+    self.tokenizer = TweetTokenizer()
 
     self.data_dir = logger.GetDropboxDrive() + '/' + logger.config_data['APP_FOLDER'] + self.folder_name
     
@@ -24,9 +28,9 @@ class EY_Data(object):
     self.generate_raw_labels()
     
     self.process_labels()
-    
-#    self.label_information(self.labels)
-    self.write_to_file()
+ 
+#    self.build_outputs()
+#    self.write_to_file()
     
   def read_files(self):
     self.answers = []
@@ -51,7 +55,7 @@ class EY_Data(object):
       self.answers.append(answer.lower())
       self.questions.append(question.lower())
       self.topics.append(topic.lower())
-      
+  
   def label_information(self, labels):
     #update flattened labels
     flattened_labels = flatten_list(labels)
@@ -93,6 +97,17 @@ class EY_Data(object):
     self.logger.P("Distribution of labels: \n {}".format(df_distrib.to_string()))
 
     return
+
+  def line_to_tags(self, line):
+    tags = []
+    tokenized_line = self.tokenizer.tokenize(line)
+    while '' in tokenized_line:
+      tokenized_line.remove('')
+    for i in tokenized_line:
+      if len(i) > 2:
+        tags.append(i)
+    
+    return tags
       
   def generate_raw_labels(self):
     self.labels = []
@@ -102,18 +117,17 @@ class EY_Data(object):
       for j in qs:
         lbl.append(self.line_to_tags(j))
         
-      
       lbl.append(self.tokenizer.tokenize(self.topics[i]))
-      
+
       lbl = flatten_list(lbl)
       lbl = list(set(lbl))
       self.labels.append(lbl)
     
-  def generate_dict_label_occ_in_docs(self):
-    self.unqiue_labels  = list(set(flatten_list(self.labels)))
+  def generate_dict_label_occ_in_docs(self, labels):
+    unqiue_labels  = list(set(flatten_list(labels)))
     dictonary = {}
-    for i in self.labels:
-      for k in self.unqiue_labels:
+    for i in labels:
+      for k in unqiue_labels:
         if k in i:
           try:
             dictonary[k] += 1
@@ -136,72 +150,104 @@ class EY_Data(object):
     self.dict_label_frequency = Counter(self.flattened_labels)
     self.common_labels = []
     
-    self.dict_label_occurence = self.generate_dict_label_occ_in_docs()
+    self.dict_label_occurence = self.generate_dict_label_occ_in_docs(self.labels)
     self.logger.P('dict label occurence: \n {}'.format(self.dict_label_occurence))
     
     #REMOVE COMMON WORDS
     for i in self.dict_label_occurence.keys():
-      percentage = self.dict_label_occurence.get(i)/len(self.answers)
-      if percentage > self.occurence_threshold:
-        self.logger.P('{} word appears in {}% of documents, will be removed from dataset'.format(i, str(percentage * 100)))
-        self.common_labels.append(i)
+      if self.occurence_threshold < 1:
+        percentage = self.dict_label_occurence.get(i)/len(self.answers)
+        if percentage > self.occurence_threshold:
+          self.logger.P('{} word appears in {}% of documents, will be removed from dataset'.format(i, str(percentage * 100)))
+          self.common_labels.append(i)
+      else:
+        if self.dict_label_occurence.get(i) >= self.occurence_threshold:
+          self.logger.P('{} word appears in {} of documents, will be removed from dataset'.format(i,self.dict_label_occurence.get(i)))
+          self.common_labels.append(i)
   
     self.logger.P('Most common tags that will be removed from list of labels:')
     for i in self.common_labels:
       self.remove_tag_from_labels(i)
     self.logger.P('Total number of removed labels {}'.format(len(self.common_labels)))
     
-    self.dict_label_occurence = self.generate_dict_label_occ_in_docs()
+    self.dict_label_occurence = self.generate_dict_label_occ_in_docs(self.labels)
     self.logger.P('dict label occurence: \n {}'.format(self.dict_label_occurence))
     
     return
   
-  def line_to_tags(self, line):
-    tags = []
-    tokenized_line = self.tokenizer.tokenize(line)
-    for i in tokenized_line:
-      if i not in self.undesirable_list:
-        tags.append(i)
+  def intersect_text_and_labels(self, text, labels):
+    found_labels = []
+    tokenized_text = self.tokenizer.tokenize(text)
+    while '' in tokenized_text:
+      tokenized_text.remove('')
+    for i in labels:
+      if i in tokenized_text:
+        found_labels.append(i)
     
-    return tags
+    return found_labels
   
   def build_outputs(self):
+    validation_index = 0
     output_texts = []
     output_labels = []
+    validation_texts = []
+    validation_labels = []
     for i in range(len(self.answers)):
       qs = self.questions[i].splitlines()
+      #populate validation set with random questions from each topic
+      if validation_index < self.validation_set_length:
+        random_q_idx = random.randint(0,len(qs) - 1)
+        validation_texts.append(qs[random_q_idx])
+        validation_labels.append(self.intersect_text_and_labels(qs[random_q_idx], self.labels[i]))
+        del qs[random_q_idx]
+        validation_index += 1
+      #remove empty questions
+      while '' in qs:
+        qs.remove('')
       for j in qs:
         output_texts.append(j)
-        output_labels.append(self.labels[i])
-      
+        #only choose relevant labels
+        lbl = self.intersect_text_and_labels(j, self.labels[i])
+        output_labels.append(lbl)
       output_texts.append(self.answers[i])
-      output_labels.append(self.labels[i])     
-  
-  
-    return output_texts, output_labels
+      #only choose relevant labels
+      lbl = self.intersect_text_and_labels(self.answers[i], self.labels[i])
+      output_labels.append(lbl)
+    
+    return output_texts, output_labels, validation_texts, validation_labels
   
   def write_to_file(self):
-    
     dir_location = self.logger.GetDropboxDrive()  + '/' + self.logger.config_data['APP_FOLDER']
-    texts, labels = self.build_outputs()
-    
+    texts, labels, valid_texts, valid_labels = self.build_outputs()
+    self.logger.P('================ information on output labels ================')
+    self.label_information(labels)
+    self.logger.P('================ end information on output labels ================')
+    #write training_data
     for i in range(len(texts)):
-      with open(dir_location + '/_output/EY_FAQ/Texts/Text_%s.txt' % i, 'w', encoding='utf-8') as f_doc:
+      with open(dir_location + '/_data/EY_FAQ/Texts/Text_%s.txt' % i, 'w', encoding='utf-8') as f_doc:
         f_doc.write(texts[i])
-
       f_doc.close()
 
-      with open(dir_location + '/_output/EY_FAQ/Labels/Labels_%s.txt' % i, 'w', encoding='utf-8') as f_label:
+      with open(dir_location + '/_data/EY_FAQ/Labels/Labels_%s.txt' % i, 'w', encoding='utf-8') as f_label:
         for j in labels[i]:
           f_label.write(j + '\n')
-      
       f_label.close()
-
+      
+      #write validation_data
+      if i < len(valid_texts):
+        with open(dir_location + '/_data/EY_FAQ/Validation_Texts/Text_%s.txt' % i, 'w', encoding='utf-8') as f_doc:
+          f_doc.write(valid_texts[i])
+        f_doc.close()
   
+        with open(dir_location + '/_data/EY_FAQ/Validation_Labels/Labels_%s.txt' % i, 'w', encoding='utf-8') as f_label:
+          for k in valid_labels[i]:
+            f_label.write(k + '\n')
+        f_label.close()
+        
 if __name__ == '__main__':
   
   logger = Logger(lib_name='EY_DATA',
                   config_file='./tagger/crawler/EY_data/config_eydata.txt',
                   TF_KERAS=False)
   
-  data = EY_Data(logger, '/_data/EY_FAQ/')
+  data = EY_Data(logger, '/_data/EY_FAQ_RAW/', occurence_threshold=3)
