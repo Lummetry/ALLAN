d837de4f67b74c5121f52d68e3ef5c338cac1614
Laurentiu Piciu <laurentiu.piciu@lummetry.ai>
2019-09-19 11:27:41 +0300

    moved find_topic logic in allan engine

--- tagger/brain/run_server.py
+++ tagger/brain/run_server.py
@@ -16,44 +16,6 @@ import pickle as pkl
 from datetime import datetime
 
 
-
-def find_topic(logger, topic_tag_map, dict_tags, choose_by_length=False):
-  """
-  Returns the topic id based on the tags found by the document tagger.
-  
-  First constructs a topic_identification map: a dictionary where all the keys
-  are the topics, and the values are lists of the tags which appear in 
-  the documents where the topic appears.
-  
-  Following that, another dictionary is constructed where the keys are the topics 
-  but the values are length of the list in the first dictionary/ sum of the probabilities.
-  
-  Returns the max value of the dictionary as the found topic
-  """
-  
-  topic_labels = list(topic_tag_map.keys())
-  topic_identification_map = {k:list() for k in topic_labels}
-  #topic identification on best tags:
-  for tag, conf in dict_tags.items():
-    for topic in topic_labels:
-      if tag in topic_tag_map[topic]:
-        topic_identification_map[topic].append((tag,conf))
-
-  if choose_by_length:
-    topic_identification_len_map = {k: len(v) for k,v in topic_identification_map.items()}
-    max_len_key = max(topic_identification_len_map, key=lambda k: topic_identification_len_map[k])
-    return max_len_key
-  
-  else:
-    topic_identification_sum_map = {k: 0 for k in topic_identification_map.keys()}
-  
-    for key, values in topic_identification_map.items():
-      topic_identification_sum_map[key] += sum([pair[1] for pair in values])
-    
-    max_sum_key = max(topic_identification_sum_map, key=topic_identification_sum_map.get)
-  
-    return max_sum_key
-
 if __name__ == '__main__':
   
   cfg1 = "tagger/brain/configs/config.txt"
@@ -80,11 +42,6 @@ if __name__ == '__main__':
   with open(topic_tag_map_path, 'rb') as handle:
     topic_tag_map = pkl.load(handle)
 
-  topic_index_map = {k:0 for k in topic_tag_map.keys()}
-  idx = 0
-  for k in topic_index_map.keys():
-    topic_index_map[k] = idx
-    idx += 1
     
   #####      
   # now load FlaskServer tools and .run()
@@ -108,14 +65,13 @@ if __name__ == '__main__':
     dic_top_best ={keys[x]:float(round(vals[x],3)) for x in top_idxs[:TOP]}
     dic_top_runner ={keys[x]:float(round(vals[x],3)) for x in top_idxs[TOP:]}
 
-    topic_document = find_topic(l, topic_tag_map, dic_top, False) #USE True to check by length    
-    id_topic_document = topic_index_map[topic_document]
+    topic_document = eng.find_topic(topic_tag_map=topic_tag_map,
+                                    dict_tags=dic_top,
+                                    choose_by_length=False) #USE True to check by length    
     str_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:22]
-    
     
     dct_info = {
         'input_document_init' : std_input,
-        'id_topic_document': id_topic_document,
         'topic_document': topic_document,
         'date_time' : str_now,
         }
