7f31be0423a9cef7bd77e66e5c0f039911c16e9b
Alexandru Ioana <alexandru.ioana@lummetry.ai>
2019-09-20 13:12:33 +0100

    doc_string documentation

--- tagger/crawler/EY_data/ey_preprocessing.py
+++ tagger/crawler/EY_data/ey_preprocessing.py
@@ -12,6 +12,11 @@ def flatten_list(a):
   return [item for sublist in a for item in sublist]
 
 class EY_Data(object):  
+  """
+  Object for preprocessing raw data of the form: questions, answers, topics (each in different files)
+  into training and validation datasets for a document tagger.
+  The tags are generated tokenizing 
+  """
   def __init__(self, logger, folder_name, occurence_threshold=0.25, validation_set_length=10):
     self.logger = logger
     self.folder_name = folder_name
@@ -111,6 +116,14 @@ class EY_Data(object):
 #    self.write_to_file()
     
   def read_files(self):
+    """
+    Reads answers, questions and topics files, from the data directory.
+    
+    Arguments: number of answer, question, topic files
+    
+    Populates the answers, questions and topic lists of the EY_Data object
+    with the content of the files.
+    """
     self.answers = []
     self.questions = []
     self.topics = []
@@ -133,8 +146,23 @@ class EY_Data(object):
       self.answers.append(answer.lower())
       self.questions.append(question.lower())
       self.topics.append(topic.lower())
+    
+    return
   
   def label_information(self, labels):
+    """
+    Prints important metrics concerning the labels list it is fed.
+    
+    Arguments: a list of lists of labels. each observation in the dataset
+               contains a list of labels. The function needs the list of lists of labels
+               
+    Prints: label frequency,
+            number of topic_labels,
+            distribution of lengths of label list in dataset,
+            labels as grouped by their frequency,
+            pandas describe distribution of labels.
+    
+    """
     #update flattened labels
     flattened_labels = flatten_list(labels)
     #update label occurence counter
@@ -181,6 +209,16 @@ class EY_Data(object):
     return
 
   def line_to_tags(self, line):
+    """
+    Constructs a list of tags based on a line(sentence).
+    uses NLTK tokenizer to generate tokens, 
+    each token is added to the list of tags 
+    IF the token is not in self.undesirable_tags
+       and the token is of length larger than 2
+    
+    
+    Arguments: line - a sentence(could have a single word)
+    """
     tags = []
     tokenized_line = self.tokenizer.tokenize(line)
     while '' in tokenized_line:
@@ -192,6 +230,15 @@ class EY_Data(object):
     return tags
       
   def generate_raw_labels(self):
+    """
+    Generates a list of lists of raw labels.
+    Each list of labels is associated to an answer, topic, question pair.
+    The raw labels are generated using the line_to_tags function.
+    
+    Another list of labels is generated soley from the topics.
+    These are unique to each label and are denoted by topic_"topic name"
+    
+    """
     self.labels = []
     self.topic_labels = []
     for i in range(len(self.questions)):
@@ -206,8 +253,19 @@ class EY_Data(object):
       lbl = flatten_list(lbl)
       lbl = list(set(lbl))
       self.labels.append(lbl)
-    
+  
+   return
+  
   def generate_dict_label_occ_in_docs(self, labels):
+    """
+    Generates a dictionary containing the occurence of each label in
+    the final dataset.
+    
+    Arguments: a list of lists of labels
+    
+    Returns: a dictionary where keys are tags, and values are their occurence values
+    
+    """
     unqiue_labels  = list(set(flatten_list(labels)))
     dictonary = {}
     for i in labels:
@@ -222,6 +280,13 @@ class EY_Data(object):
   
       
   def remove_tag_from_labels(self, tag):
+    """
+    A function meant to remove a single tag from all sublists of
+    the self.labels list of lists.
+    
+    Arguments: an item that can be found in self.labels
+    
+    """
     self.logger.P('Removing tag {} from all labels...'.format(tag))
     new_labels = []
     for i in self.labels:
@@ -229,7 +294,15 @@ class EY_Data(object):
       
     self.labels = new_labels
     
+    return
+
   def replace_tags(self, replacement_tag, tag_to_replace):
+    """
+    Replaces a single tag with a different tag in self.labels
+    
+    Arguments: tag_to_replace: the label which is going to be replaced
+               replacement_tag: the label which is going to replace
+    """
     new_labels = []
     for row in self.labels:
       new_row = []
@@ -248,6 +321,15 @@ class EY_Data(object):
     self.labels = new_labels
     
   def replace_tags_in_row(self, row, replacement, tag_to_replace):
+    """
+    Replaces a single tag in a single list of labels
+    
+    Arguments: row: list of labels
+               replacement: label which will replace 
+               tag_to_replace: label which will be replaced
+               
+    Returns a list of labels where the replacement is applied
+    """
     new_row = []
     for item in row:
       if item == tag_to_replace:
@@ -258,6 +340,15 @@ class EY_Data(object):
     return new_row
     
   def process_labels(self):
+    """
+    Function that removes labels that occur in a large enough percentage of the observations
+    
+    Meant to remove noise from the distribution of labels, with the assumption that
+    the most meaningful tags are the ones that occur less frequently.
+    
+    The percentage of occurence of each tag is computed and if it is above the
+    occurence threshold as initialized, it will be removed from all lists of labels.
+    """
     self.flattened_labels = flatten_list(self.labels)
 
     self.dict_label_frequency = Counter(self.flattened_labels)
@@ -297,6 +388,16 @@ class EY_Data(object):
     return
   
   def build_topic_label_map(self):
+    """
+    Builds a mapping(dictionary) between topic_labels, and all other labels.
+    
+    The topic_labels are unique to each topic/question/answer pairs, but the other
+    labels are not always unique to a single topic. The function builds a 
+    dictionary that visualizes that mapping.
+    
+    The dictionary is written do disk as it is needed by the document tagger 
+    for training.
+    """
     self.topic_label_map = dict.fromkeys(self.topic_labels, [])
     labels_copy = self.labels
     topic_label_map_values = []
@@ -315,6 +416,13 @@ class EY_Data(object):
       pickle.dump(self.topic_label_map, file)
   
   def build_tag2idx_map(self):
+    """
+    Creates a dictionary indexing the labels.
+    
+    Each label is attributed a number, number which is incremented atomically.
+    
+    The mapping is printed on the screen.
+    """
     dic_lbl2idx = {}
     index = 0 
     for tags in self.topic_label_map.values():
@@ -325,7 +433,21 @@ class EY_Data(object):
         
     for k,v in dic_lbl2idx.items():
       print('{} : "{}"'.format(k,v))
+      
   def intersect_text_and_labels(self, text, labels):
+    """
+    Function that intersects the text of an observation with the full list of 
+    tags for the topic/question/answer pair that the observation is a part of.
+    
+    Additionally, labels are simplified by using the self.dict_lbl_simpler mapping
+    
+    Arguments: text: a string(sentence) that is a part of the dataset
+               labels: the whole list of tags of the topic 
+    
+    Returns: a smaller list of labels populated by the:
+             topic_label of the observation, and any additional tags that from 
+             the whole list of labels that also appears in the text
+    """
     found_labels = []
     found = 0
     flattened_values = flatten_list(list(self.dict_lbl_simpler.values()))
@@ -348,6 +470,21 @@ class EY_Data(object):
     return found_labels
   
   def build_outputs(self):
+    """
+    Function that builds the dataset.
+    Iterates through topic/questions/answers set, splits questions into individual 
+    observations.
+    
+    Builds a training set and a validation set of size: self.validation_set_length
+    Each observation initially contains the full labels for the topic, but its 
+    label list gets reduced with intersect_text_and_labels.
+    
+    Returns: output_texts: list of text observations
+             output_labels: list of labels for each text
+             validation_texts: list of validation texts
+             validation_labels: list of  labels for each validation text
+    
+    """
     validation_index = 0
     output_texts = []
     output_labels = []
@@ -402,6 +539,10 @@ class EY_Data(object):
     return output_texts, output_labels, validation_texts, validation_labels
   
   def write_to_file(self):
+    """
+    Writes to file the training and validation dataset.
+    
+    """
     dir_location = self.logger.GetDropboxDrive()  + '/' + self.logger.config_data['APP_FOLDER']
     texts, labels, valid_texts, valid_labels = self.build_outputs()
     self.logger.P('================ information on output labels ================')
@@ -432,6 +573,15 @@ class EY_Data(object):
         f_label.close()
 
   def get_entries(self, fn_questions):
+    """
+    Parses the txt files with tests returned by EY
+    
+    Arguments: fn_questions: filename of the document containing the texts
+    
+    Returns: a list of entries where an entry is the string 
+             between curly brackets containing the information of the tagger's 
+             return
+    """
     folder = self.data_dir[:-1].rsplit('/',1)[0]
     list_of_entries = []
     entry = []
@@ -450,16 +600,34 @@ class EY_Data(object):
     return list_of_entries
   
   def parse_score(self, score):
-      if 'X' in score:
-          score = score.strip('X')
-      if '}' in score:
-        score = score.strip('}')
-      
-      score = float(score)
-      return score
+    """
+    A function that cleans and turns the scores for a label into float
+    
+    Since the EY testing team marked "XX" wherever the tagger was wrong,
+    to get the actual values returned by the tagger it is necessary to remove
+    any additional string.
+    
+    Returns a float score of the label
+    """
+    if 'X' in score:
+        score = score.strip('X')
+    if '}' in score:
+      score = score.strip('}')
+    
+    score = float(score)
+    return score
     
   
   def get_tags(self, entry):
+    """
+    Processes an entry and returns its information.
+    
+    Arguments: a single entry from the list of entries as returned by get_entries
+    
+    Returns: proc_query: the processed query of the entry(what allan sees)
+             query: the unprocessed query of the entry(raw input)
+             tags: a list of tuples containing label name and float score
+    """
     self.flattened_labels = flatten_list(self.labels)
     tags = []
     for line in entry:
@@ -490,6 +658,16 @@ class EY_Data(object):
     return proc_query, query, tags
   
   def find_topic_in_map(self, topic_map):
+    """
+    Finds the topic of a topic_map
+    
+    Arugments: topic_map: a dictionary where keys: topic_labels and values are 
+                      the tags that appear in the topic(as in topic_label_map)
+    
+    Returns: max_len_key: the topic where the list of tags contained is the longest
+             max_sum_key: the topic where the sum of list of tags is the largest
+             
+    """
     max_len_key = ''
     max_sum_key = ''
     topic_identification_len_map = {k: len(v) for k,v in topic_map.items()}
@@ -501,15 +679,27 @@ class EY_Data(object):
     max_len_key = max(topic_identification_len_map, key=lambda k: topic_identification_len_map[k])
     max_sum_key = max(topic_identification_sum_map, key=topic_identification_sum_map.get)
 
-#    self.logger.P('The identifcation maps:')
-#    for k,v in topic_map.items():
-#      if len(v) > 0:
-#        self.logger.P('{}: length = {} sum = {} -- {}'.format(k, topic_identification_len_map[k], topic_identification_sum_map[k], v))
-#    
+    self.logger.P('The identifcation maps:')
+    for k,v in topic_map.items():
+      if len(v) > 0:
+        self.logger.P('{}: length = {} sum = {} -- {}'.format(k, topic_identification_len_map[k], topic_identification_sum_map[k], v))
+    
     return max_len_key, max_sum_key
 
 
   def best_topic(self, entry):
+    """
+    Function that generates topic_map of an entry and returns final topic
+    
+    Arguments: a single entry from the list of entries as returned by get_entries
+    
+    Generates a topic_map: a dictionary where keys: topic_labels and values are 
+    the tags that appear in the topic(as in topic_label_map), and applies 
+    find_topic_in_map on the generated topic_map
+    
+    Prints valuable information for the entry: query, proc_query(what allan sees),
+    max len topic, max sum topic.
+    """
     proc_query, query, tags = self.get_tags(entry)
     print()
     topic_identification_map = {k:list() for k in self.topic_labels}
@@ -533,9 +723,6 @@ class EY_Data(object):
     max_len_topic, max_sum_topic = self.find_topic_in_map(topic_identification_map)
     self.logger.P('Max length topic : {}'.format(max_len_topic), noprefix=True)
     self.logger.P('Max sum topic : {}'.format(max_sum_topic), noprefix=True)
-    self.logger.P('Correct topic:', noprefix=True)
-    self.logger.P('In training data?:', noprefix=True)
-    self.logger.P('Query quality:', noprefix=True)
     
     return 
 
@@ -546,6 +733,8 @@ if __name__ == '__main__':
                   TF_KERAS=False)
   
   data = EY_Data(logger, '/_data/EY_FAQ_RAW2/', occurence_threshold=0.25)
+  
+  #UNCOMMENT THIS BIT TO GET RESUTLS OF EY tests, change file argument for new tests
 #  list_of_entries = data.get_entries('/ALLEN_Wrong_Questions_2.txt')
 #
 #  for entry in list_of_entries:
